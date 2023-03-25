236496b51eeb2f0bb50b32e5a455289878c1281d
nagaraju9951 <you@example.com>
2023-03-25 22:02:15 +0530

    updated ec2 module

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
-}
-resource "aws_instance" "my-ec2-instance" {
-    ami = "${var.ami_id}"
-    instance_type = "${var.instance_type}"
- }
 
+resource "aws_instance" "demo_ec2" {
+  ami                    = "ami-0557a15b87f6559cf"
+  instance_type          = "t2.micro"
+  key_name               = "demokey"
+  vpc_security_group_ids = ["sg-02e9238f2557e4bef"]
+  subnet_id              = "subnet-061c5aa91120d6221"
+
+  tags = {
+    Name = "terrform-demo-ec2"
+    Environment = "training"
+  }
+}
