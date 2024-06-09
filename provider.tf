52cbbc0627540a71dafaddd3c068a889da420d20
nagaraju9951 <126653784+nagaraju9951@users.noreply.github.com>
2024-06-09 12:35:34 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    bucket         = "snr-us-east-1-tfstates"
+    bucket         = "mk-1-tk-1nh"
   
   }
 }
