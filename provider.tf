313d6361dc0fe74d002ad0a00155916cf03bc314
nagaraju9951 <126653784+nagaraju9951@users.noreply.github.com>
2024-06-09 13:11:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = var.region
 }
 
-terraform {
-  backend "s3" {
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    bucket         = "mk-1-tk-1nh"
+#terraform {
+ # backend "s3" {
+  #  key            = "terraform.tfstate"
+   # region         = "ap-south-1"
+    #bucket         = "mk-1-tk-1nh"
   
-  }
-}
+  #}
+#}
 
